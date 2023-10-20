548c24724f1824defe5046f50fe79be6d4170e98
Kyle Brindley <kbrindley@lanl.gov>
2023-10-19 20:18:38 -0600

    WIP: reproduce the abaqus python interface in python 3

---
+++ turbo_turtle/main.py
@@ -0,0 +1,47 @@
+import argparse
+
+
+def get_parser():
+
+    default_center = [0.0, 0.0, 0.0]
+    default_xpoint = [1.0, 0.0, 0.0]
+    default_zpoint = [0.0, 0.0, 1.0]
+    default_plane_angle = 45.0
+    default_partitions_x = [0.0, 0.0]
+    default_partitions_y = [0.0, 0.0]
+    default_partitions_z = [0.0, 0.0]
+
+    cli_description = "Partition a spherical shape into a turtle shell given a small number of locating parameters."
+
+    parser = argparse.ArgumentParser(description=cli_description)
+
+    parser.add_argument('--xpoint', nargs=3, type=float, default=default_xpoint,
+                        help="Point on the x-axis (default: %(default)s)")
+    parser.add_argument('--center', nargs=3, type=float, default=default_center,
+                        help="Center of the sphere (default: %(default)s)")
+    parser.add_argument('--zpoint', nargs=3, type=float, default=default_zpoint,
+                        help="Point on the z-axis (default: %(default)s)")
+    parser.add_argument('--plane-angle', type=float, default=default_plane_angle,
+                        help="Angle for non-principal partitions (default: %(default)s)")
+    parser.add_argument('--x-partitions', type=float, nargs='+', default=default_partitions_x,
+                        help="Create a partition offset from the x-principal-plane (default: %(default)s)")
+    parser.add_argument('--y-partitions', type=float, nargs='+', default=default_partitions_y,
+                        help="Create a partition offset from the y-principal-plane (default: %(default)s)")
+    parser.add_argument('--z-partitions', type=float, nargs='+', default=default_partitions_z,
+                        help="Create a partition offset from the z-principal-plane (default: %(default)s)")
+
+    requiredNamed = parser.add_argument_group('Required Named Arguments')
+    requiredNamed.add_argument('--model-name', type=str, required=True,
+                        help="Abaqus model name")
+    requiredNamed.add_argument('--part-name', type=str, required=True,
+                        help="Abaqus part name")
+    requiredNamed.add_argument('--input-file', type=str, required=True,
+                        help="Abaqus model database to open")
+    parser.add_argument('--output-file', type=str, default=None,
+                        help="Abaqus model database to save to. Defaults to the specified --input-file")
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args, unknown = parser.parse_known_args()
