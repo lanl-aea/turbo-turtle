3f82d56f06ff042b0567674e498f7e78d3364bb8
Kyle Brindley <kbrindley@lanl.gov>
2023-10-20 21:23:45 -0600

    WIP: packaging script from WAVES

---
+++ package_documentation.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import pathlib
+
+prefix = pathlib.Path(os.getenv("PREFIX")).resolve()
+sp_dir = pathlib.Path(os.getenv("SP_DIR")).resolve()
+pkg_name = os.getenv("PKG_NAME")
+
+man_path = pathlib.Path("build/docs/man/turbo-turtle.1").resolve()
+html_path_external = pathlib.Path("build/docs/html").resolve()
+# FIXME: setuptools claims that these should be included by default, but they aren't
+readme_path = pathlib.Path("README.rst").resolve()
+pyproject = pathlib.Path("pyproject.toml").resolve()
+
+new_paths = [
+    (prefix / "share/man/man1", man_path),
+    (prefix / "man/man1", man_path),
+    (sp_dir / pkg_name / "docs", html_path),
+    (sp_dir / pkg_name / "README.rst", readme_path),
+    (sp_dir / pkg_name / "pyproject.toml", pyproject),
+]
+for destination, source in new_paths:
+    assert source.exists()
+    print(f"Copying '{source}' to '{destination}'...")
+    if source.is_file():
+        destination.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy2(source, destination, follow_symlinks=True)
+    else:
+        destination.mkdir(parents=True, exist_ok=True)
+        shutil.copytree(source, destination, symlinks=False, dirs_exist_ok=True,
+                        ignore=shutil.ignore_patterns(".doctrees", "*.doctree", ".buildinfo"))
