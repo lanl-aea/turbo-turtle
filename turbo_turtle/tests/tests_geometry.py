2e07a44a0fca0b383c696ca30ae0c41c5d4e558e
Kyle Brindley <kbrindley@lanl.gov>
2023-10-23 16:02:45 -0600

    WIP: add file handling to the sphere script. an output file is always necessary, but we don't require an input file because we want to create models from scratch if necessary

--- turbo_turtle/tests/tests_geometry.py
+++ turbo_turtle/tests/tests_geometry.py
@@ -1,5 +1,7 @@
+import os
 import sys
 import numpy
+import shutil
 
 import abaqus
 import abaqusConstants
@@ -22,21 +24,35 @@ def main(model_name, output_file):
     swiss_cheese(model_name)
 
 
-def sphere(inner_radius, outer_radius, output_file, quadrant="both", model_name="Model-1", part_name='wedge-sphere', angle=360., center=(0., 0.)):
+def sphere(inner_radius, outer_radius, output_file, input_file=None, quadrant="both", angle=360., center=(0., 0.),
+           model_name="Model-1", part_name='sphere'):
     """Create a hollow, spherical geometry with both upper (+Y) and lower (-Y) quadrants
 
     :param float inner_radius: inner radius (size of hollow)
     :param float outer_radius: outer radius (size of sphere)
+    :param str output_file: output file name. Will be stripped of the extension and ``.cae`` will be used.
+    :param str input_file: input file name. Will be stripped of the extension and ``.cae`` will be used.
     :param str quadrant: quadrant of XY plane for the sketch: upper (I), lower (IV), both
-    :param str model_name: name of the Abaqus model
-    :param str part_name: name of the part to be created in the Abaqus model
     :param float angle: angle of rotation 0.-360.0 degrees.
     :param tuple center: tuple of floats (X, Y) location for the center of the sphere
+    :param str model_name: name of the Abaqus model
+    :param str part_name: name of the part to be created in the Abaqus model
     """
     quadrant_options = ("both", "upper", "lower")
     if not quadrant in quadrant_options:
         sys.stderr.write("Quadrant option must be one of: {}".format(quadrant_options))
         sys.exit(1)
+
+    # Avoid modifying the contents or timestamp on the input file.
+    # Required to get conditional re-builds with a build system such as GNU Make, CMake, or SCons
+    output_file = os.path.splitext(output_file)[0]
+    if input_file is None:
+        input_file = output_file
+    input_file = os.path.splitext(input_file)[0]
+    input_with_extension = '{}.cae'.format(input_file)
+    output_with_extension = '{}.cae'.format(output_file)
+    if input_file != output_file:
+        shutil.copyfile(input_with_extension, output_with_extension)
 
     if not model_name in abaqus.mdb.models.keys():
         abaqus.mdb.Model(name=model_name, modelType=abaqusConstants.STANDARD_EXPLICIT)
