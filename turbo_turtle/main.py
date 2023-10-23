cc25a326f7b186c495d2729124c00ac8c2b33f20
Kyle Brindley <kbrindley@lanl.gov>
2023-10-23 17:25:44 -0600

    TST: functional system test update to use sphere subcommand

--- turbo_turtle/main.py
+++ turbo_turtle/main.py
@@ -222,7 +222,7 @@ def main():
     args, unknown = parser.parse_known_args()
     if args.subcommand == "sphere":
         _sphere(args)
-    if args.subcommand == "partition":
+    elif args.subcommand == "partition":
         _partition(args)
     elif args.subcommand == "docs":
         _docs(print_local_path=args.print_local_path)
