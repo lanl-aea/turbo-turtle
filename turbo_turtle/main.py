c6690ea33ab06e7619e1c01ba4176aac6c29e9f5
Kyle Brindley <kbrindley@lanl.gov>
2023-11-07 11:54:04 -0700

    MAINT: remove duplicated partition parser

--- turbo_turtle/main.py
+++ turbo_turtle/main.py
@@ -92,54 +92,6 @@ def _sphere(args):
     stdout = subprocess.check_output(command)
 
 
-# TODO: write a Python 2/3 compatible parser and argument handler
-def _partition_parser():
-    """Get parser object for partition subcommand command line options
-
-    :return: parser
-    :rtype: ArgumentParser
-    """
-
-    default_center = [0.0, 0.0, 0.0]
-    default_xpoint = [1.0, 0.0, 0.0]
-    default_zpoint = [0.0, 0.0, 1.0]
-    default_plane_angle = 45.0
-    default_partitions_x = [0.0, 0.0]
-    default_partitions_y = [0.0, 0.0]
-    default_partitions_z = [0.0, 0.0]
-
-    parser = argparse.ArgumentParser(add_help=False)
-
-    requiredNamed = parser.add_argument_group('Required Named Arguments')
-    requiredNamed.add_argument('--model-name', type=str, required=True,
-                               help="Abaqus model name")
-    requiredNamed.add_argument('--part-name', type=str, required=True,
-                               help="Abaqus part name")
-    requiredNamed.add_argument('--input-file', type=str, required=True,
-                               help="Abaqus model database to open")
-
-    parser.add_argument('--output-file', type=str, default=None,
-                        help="Abaqus model database to save to. Defaults to the specified --input-file")
-    parser.add_argument('--xpoint', nargs=3, type=float, default=default_xpoint,
-                        help="Point on the x-axis (default: %(default)s)")
-    parser.add_argument('--center', nargs=3, type=float, default=default_center,
-                        help="Center of the sphere (default: %(default)s)")
-    parser.add_argument('--zpoint', nargs=3, type=float, default=default_zpoint,
-                        help="Point on the z-axis (default: %(default)s)")
-    parser.add_argument('--plane-angle', type=float, default=default_plane_angle,
-                        help="Angle for non-principal partitions (default: %(default)s)")
-    parser.add_argument('--x-partitions', type=float, nargs='+', default=default_partitions_x,
-                        help="Create a partition offset from the x-principal-plane (default: %(default)s)")
-    parser.add_argument('--y-partitions', type=float, nargs='+', default=default_partitions_y,
-                        help="Create a partition offset from the y-principal-plane (default: %(default)s)")
-    parser.add_argument('--z-partitions', type=float, nargs='+', default=default_partitions_z,
-                        help="Create a partition offset from the z-principal-plane (default: %(default)s)")
-
-    parser.add_argument('--abaqus-command', type=str, default=_settings._default_abaqus_command,
-                        help='Abaqus executable absolute or relative path (default: %(default)s)')
-    return parser
-
-
 def _partition(args):
     """Python 3 wrapper around the Abaqus Python partition CLI
 
