d7b17d7ec368a369d3545bcbac9f7161de9f6bcc
Kyle Brindley <kbrindley@lanl.gov>
2023-11-11 15:28:22 -0700

    WIP: placeholder cubit wrappers

---
+++ turbo_turtle/_cubit_wrappers.py
@@ -0,0 +1,75 @@
+import subprocess
+
+from turbo_turtle import _settings
+
+
+def geometry(args, command):
+    """Python 3 wrapper around Cubit
+
+    :param argparse.Namespace args: namespace of parsed arguments
+    :param str command: cubit executable path
+    """
+    sys.exit("Not yet implemented")
+
+
+def cylinder(args, command):
+    """Python 3 wrapper around Cubit
+
+    :param argparse.Namespace args: namespace of parsed arguments
+    :param str command: cubit executable path
+    """
+    sys.exit("Not yet implemented")
+
+
+def sphere(args, command):
+    """Python 3 wrapper around Cubit
+
+    :param argparse.Namespace args: namespace of parsed arguments
+    :param str command: cubit executable path
+    """
+    sys.exit("Not yet implemented")
+
+
+def partition(args, command):
+    """Python 3 wrapper around Cubit
+
+    :param argparse.Namespace args: namespace of parsed arguments
+    :param str command: cubit executable path
+    """
+    sys.exit("Not yet implemented")
+
+
+def mesh(args, command):
+    """Python 3 wrapper around Cubit
+
+    :param argparse.Namespace args: namespace of parsed arguments
+    :param str command: cubit executable path
+    """
+    sys.exit("Not yet implemented")
+
+
+def merge(args, command):
+    """Python 3 wrapper around Cubit
+
+    :param argparse.Namespace args: namespace of parsed arguments
+    :param str command: cubit executable path
+    """
+    sys.exit("Not yet implemented")
+
+
+def export(args, command):
+    """Python 3 wrapper around Cubit
+
+    :param argparse.Namespace args: namespace of parsed arguments
+    :param str command: cubit executable path
+    """
+    sys.exit("Not yet implemented")
+
+
+def image(args, command):
+    """Python 3 wrapper around Cubit
+
+    :param argparse.Namespace args: namespace of parsed arguments
+    :param str command: cubit executable path
+    """
+    sys.exit("Not yet implemented")
