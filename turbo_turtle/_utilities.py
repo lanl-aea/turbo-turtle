eb5b9b73ea5e37e3e3f3c9a15ef6dc8879052e15
Kyle Brindley <kbrindley@lanl.gov>
2023-11-13 08:40:41 -0700

    MAINT: re-use python 3 compatible functions from the abaqus python utilities module

--- turbo_turtle/_utilities.py
+++ turbo_turtle/_utilities.py
@@ -4,6 +4,8 @@ import pathlib
 import inspect
 import functools
 
+from turbo_turtle._abaqus_python._utilities import print_exception_message
+
 
 def search_commands(options):
     """Return the first found command in the list of options. Return None if none are found.
@@ -32,21 +34,6 @@ def find_command(options):
     if command_abspath is None:
         raise FileNotFoundError(f"Could not find any executable on PATH in: {', '.join(options)}")
     return command_abspath
-
-
-def print_exception_message(function):
-    """Decorate a function to catch any exception, print the message and call sys.exit
-
-    :param function: function to decorate
-    """
-    @functools.wraps(function)
-    def wrapper(*args, **kwargs):
-        try:
-            output = function(*args, **kwargs)
-        except Exception as err:
-            sys.exit(str(err))
-        return output
-    return wrapper
 
 
 @print_exception_message
