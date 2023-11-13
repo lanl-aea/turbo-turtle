75596692039ed7addc069ff86e0021b5200a409c
Kyle Brindley <kbrindley@lanl.gov>
2023-11-13 11:35:59 -0700

    TST: flesh out test

--- turbo_turtle/_abaqus_python/test_abaqus_utilities.py
+++ turbo_turtle/_abaqus_python/test_abaqus_utilities.py
@@ -7,13 +7,16 @@ filename = inspect.getfile(lambda: None)
 basename = os.path.basename(filename)
 parent = os.path.dirname(filename)
 sys.path.insert(0, parent)
-import _utilities
+import _abaqus_utilities
+
+import abaqusConstants
 
 
 class TestAbaqusUtilities(unittest.TestCase):
 
-    def test_abaqus_constant(self):
-        assert True
+    def test_return_abaqus_constant(self):
+        attribute = _abaqus_utilities.return_abaqus_constant("C3D8")
+        assert attribute == abaqusConstants.C3D8
 
 
 if __name__ == '__main__':
