c065d61d3cb5290f379d11bc456911b1e7c065f4
Kyle Brindley <kbrindley@lanl.gov>
2023-11-13 11:02:36 -0700

    WIP: demo case for testing Abaqus Python

---
+++ turbo_turtle/_abaqus_python/test_abaqus_utilities.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import inspect
+import unittest
+
+filename = inspect.getfile(lambda: None)
+basename = os.path.basename(filename)
+parent = os.path.dirname(filename)
+sys.path.insert(0, parent)
+import _utilities
+
+
+class TestAbaqusUtilities(unittest.TestCase):
+
+    def test_abaqus_constant(self):
+        assert False
+
+
+if __name__ == '__main__':
+    unittest.main()
