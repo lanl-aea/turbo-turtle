e3f4395b2b0e955071594fe9344ee2638923c461
Kyle Brindley <kbrindley@lanl.gov>
2023-11-13 20:50:53 -0700

    WIP: unpack the arguments explicitly into a function that runs cubit

--- turbo_turtle/_cubit_wrappers.py
+++ turbo_turtle/_cubit_wrappers.py
@@ -4,7 +4,8 @@ import pathlib
 import cubit
 
 from turbo_turtle._abaqus_python import _mixed_utilities
-from turbo_turtle._abaqus_python import vertices 
+from turbo_turtle._abaqus_python import vertices
+from turbo_turtle._abaqus_python import parsers
 
 
 def geometry(args, command):
@@ -17,14 +18,50 @@ def geometry(args, command):
     :param argparse.Namespace args: namespace of parsed arguments
     :param str command: cubit executable path
     """
+    _geometry(args.input_file, args.output_file,
+              planar=args.geometry_default_planar,
+              model_name=args.geometry_default_model_name,
+              part_name=args.geometry_default_part_name,
+              unit_conversion=args.geometry_default_unit_conversion,
+              euclidean_distance=args.geometry_default_euclidean_distance,
+              delimiter=args.geometry_default_delimiter,
+              header_lines=args.geometry_default_header_lines,
+              revolution_angle=args.geometry_default_revolution_angle)
+
+
+def _geometry(input_file, output_file,
+              planar=parsers.geometry_default_planar,
+              model_name=parsers.geometry_default_model_name,
+              part_name=parsers.geometry_default_part_name,
+              unit_conversion=parsers.geometry_default_unit_conversion,
+              euclidean_distance=parsers.geometry_default_euclidean_distance,
+              delimiter=parsers.geometry_default_delimiter,
+              header_lines=parsers.geometry_default_header_lines,
+              revolution_angle=parsers.geometry_default_revolution_angle):
+    """Create geometry from an array of XY coordiantes
+
+    :param str input_file: input text file(s) with coordinates to draw
+    :param str output_file: Abaqus CAE database to save the part(s)
+    :param bool planar: switch to indicate that 2D model dimensionality is planar, not axisymmetric
+    :param str model_name: name of the Abaqus model in which to create the part
+    :param list part_name: name(s) of the part(s) being created
+    :param float unit_conversion: multiplication factor applies to all coordinates
+    :param float euclidean_distance: if the distance between two coordinates is greater than this, draw a straight line.
+        Distance should be provided in units *after* the unit conversion
+    :param str delimiter: character to use as a delimiter when reading the input file
+    :param int header_lines: number of lines in the header to skip when reading the input file
+    :param float revolution_angle: angle of solid revolution for ``3D`` geometries
+
+    :returns: writes ``{output_file}.cae``
+    """
     cubit.init(["cubit", "-nojournal"])
-    part_name = _mixed_utilities.validate_part_name_or_exit(args.input_file, args.part_name)
-    output_file = pathlib.Path(args.output_file).with_suffix(".cub")
-    for file_name, new_part in zip(args.input_file, part_name):
-        coordinates = _mixed_utilities.return_genfromtxt(file_name, args.delimiter, args.header_lines,
+    part_name = _mixed_utilities.validate_part_name_or_exit(input_file, part_name)
+    output_file = pathlib.Path(output_file).with_suffix(".cub")
+    for file_name, new_part in zip(input_file, part_name):
+        coordinates = _mixed_utilities.return_genfromtxt(file_name, delimiter, header_lines,
                                                          expected_dimensions=2, expected_columns=2)
-        coordinates = coordinates * args.unit_conversion
-        lines, splines = vertices.lines_and_splines(coordinates, args.euclidean_distance)
+        coordinates = coordinates * unit_conversion
+        lines, splines = vertices.lines_and_splines(coordinates, euclidean_distance)
         for point1, point2 in lines:
             point1_text = " ".join(map(str, point1)) + " 0"
             point2_text = " ".join(map(str, point2)) + " 0"
