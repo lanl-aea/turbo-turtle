767bb0267d4d6db12bcab684373eb12ee13aaadf
Kyle Brindley <kbrindley@lanl.gov>
2023-11-13 20:37:40 -0700

    WIP: address all the little variable/namespace issues

--- turbo_turtle/_cubit_wrappers.py
+++ turbo_turtle/_cubit_wrappers.py
@@ -1,8 +1,10 @@
 import sys
+import pathlib
 
 import cubit
 
-from turbo_turtle._abaqus_python import _mixed_utilties
+from turbo_turtle._abaqus_python import _mixed_utilities
+from turbo_turtle._abaqus_python import vertices 
 
 def geometry(args, command):
     """Python 3 wrapper around Cubit
@@ -15,18 +17,18 @@ def geometry(args, command):
     :param str command: cubit executable path
     """
     cubit.init(["cubit", "-nojournal"])
-    part_name = _mixed_utilities.validate_part_name_or_exit(input_file, part_name)
-    output_file = pathlib.Path(output_file).with_suffix(".cub")
-    for file_name, new_part in zip(input_file, part_name):
+    part_name = _mixed_utilities.validate_part_name_or_exit(args.input_file, args.part_name)
+    output_file = pathlib.Path(args.output_file).with_suffix(".cub")
+    for file_name, new_part in zip(args.input_file, part_name):
         coordinates = _mixed_utilities.return_genfromtxt(file_name, args.delimiter, args.header_lines,
                                                          expected_dimensions=2, expected_columns=2)
         coordinates = coordinates * args.unit_conversion
-        lines, splines = vertices.lines_and_splines(coordinates, euclidean_distance)
+        lines, splines = vertices.lines_and_splines(coordinates, args.euclidean_distance)
         for point1, point2 in lines:
             point1_text = " ".join(map(str, point1)) + " 0"
             point2_text = " ".join(map(str, point1)) + " 0"
-            cubit.command("create curve location {point1_text} location {point2_text}")
-    cubit.command("save as '{output_file}' overwrite")
+            cubit.cmd("create curve location {point1_text} location {point2_text}")
+    cubit.cmd("save as '{output_file}' overwrite")
 
 
 def cylinder(args, command):
