760d192747ce51e2422715d1430cfcee45ec6ef3
Kyle Brindley <kbrindley@lanl.gov>
2023-11-15 10:19:18 -0700

    WIP: some system tests require a series of commands prior to temp directory cleanup. Run the legacy geometry system tests

--- turbo_turtle/tests/test_systemtest.py
+++ turbo_turtle/tests/test_systemtest.py
@@ -15,6 +15,7 @@ subcommand_list = parser._subparsers._group_actions[0].choices.keys()
 
 env = os.environ.copy()
 turbo_turtle_command = "turbo-turtle"
+legacy_geometry_file = "legacy_geometry.py"
 
 try:
     version("turbo_turtle")
@@ -25,6 +26,7 @@ except PackageNotFoundError:
 # If executing in repository, add package to PYTHONPATH and change the root command
 if not installed:
     turbo_turtle_command = "python -m turbo_turtle.main"
+    legacy_geometry_file = _settings._project_root_abspath.parent / legacy_geometry_file
     package_parent_path = _settings._project_root_abspath.parent
     key = "PYTHONPATH"
     if key in env:
@@ -32,18 +34,30 @@ if not installed:
     else:
         env[key] = f"{package_parent_path}"
 
-command_list = [f"{turbo_turtle_command} -h"]
-command_list.extend([f"{turbo_turtle_command} {subcommand} -h" for subcommand in subcommand_list])
+commands_list = [f"{turbo_turtle_command} -h"]
+commands_list.extend([f"{turbo_turtle_command} {subcommand} -h" for subcommand in subcommand_list])
+# Legacy geometry system tests requires a series of commands before the temp directory is removed
+name='Turbo-Turtle-Tests'
+commands_list.append([
+    f"abq2023 cae -noGui {legacy_geometry_file}",
+    f"{turbo_turtle_command} partition --input-file {name}.cae --output-file {name}.cae --model-name {name} --part-name seveneigths-sphere --center 0 0 0 --xpoint 1 0 0 --zpoint 0 0 1 --plane-angle 45",
+    f"{turbo_turtle_command} image --input-file {name}.cae --model-name {name} --output-file seveneigths-sphere.png --part-name seveneigths-sphere",
+    f"{turbo_turtle_command} partition --input-file {name}.cae --output-file {name}.cae --model-name {name} --part-name swiss-cheese --center 0 0 0 --xpoint 1 0 0 --zpoint 0 0 1 --plane-angle 45",
+    f"{turbo_turtle_command} image --input-file {name}.cae --model-name {name} --output-file swiss-cheese.png --part-name swiss-cheese",
+])
 
 @pytest.mark.systemtest
-@pytest.mark.parametrize("command", command_list)
-def test_run_tutorial(command):
+@pytest.mark.parametrize("commands", commands_list)
+def test_run_tutorial(commands):
     """Run the system tests.
 
     Executes with a temporary directory that is cleaned up after each test execution.
 
     :param str command: the full command string for the system test
     """
+    if isinstance(commands, str):
+        commands = [commands]
     with tempfile.TemporaryDirectory() as temp_directory:
-        command = command.split(" ")
-        result = subprocess.check_output(command, env=env, cwd=temp_directory).decode('utf-8')
+        for command in commands:
+            command = command.split(" ")
+            result = subprocess.check_output(command, env=env, cwd=temp_directory).decode('utf-8')
