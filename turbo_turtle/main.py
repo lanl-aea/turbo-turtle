6098273323bcf5336fbbee19dbda4357339549b4
Kyle Brindley <kbrindley@lanl.gov>
2023-11-21 15:28:08 -0700

    WIP: sys.path doesn't like pathlib.Path objects

--- turbo_turtle/main.py
+++ turbo_turtle/main.py
@@ -200,7 +200,7 @@ def main():
         command = None
         import importlib.util
         if importlib.util.find_spec("cubit") is None:
-            sys.path.append(_utilities.find_cubit_bin(args.cubit_command))
+            sys.path.append(str(_utilities.find_cubit_bin(args.cubit_command)))
         from turbo_turtle import _cubit_wrappers as _wrappers
     elif "abaqus_command" in vars(args).keys():
         command = _utilities.find_command_or_exit(args.abaqus_command)
