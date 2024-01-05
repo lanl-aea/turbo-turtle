c19e41ea75fb152c9e2a601333eb2d6dc21e232c
Kyle Brindley <kbrindley@lanl.gov>
2024-01-05 10:35:03 -0700

    WIP: some waves internals aren't deployed on conda-forge yet. hardcode as project settings

--- turbo_turtle/_settings.py
+++ turbo_turtle/_settings.py
@@ -8,3 +8,7 @@ _abaqus_python_abspath = _project_root_abspath / "_abaqus_python"
 _installed_docs_index = _project_root_abspath / "docs/index.html"
 _default_abaqus_options = ["abaqus", "abq2023"]
 _default_cubit_options = ["cubit"]
+
+# Copy from WAVES because some settings aren't available on conda-forge yet.
+_cd_action_prefix = 'cd ${TARGET.dir.abspath} &&'
+_redirect_action_postfix = "> ${TARGETS[-1].abspath} 2>&1"
