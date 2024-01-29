868b6486d9ac1871e86f79948e7fd9cbd8883e22
Kyle Brindley <kbrindley@lanl.gov>
2024-01-29 13:49:52 -0700

    TST: add abaqus Python 2 unit tests for the euclidean distance comparison

--- turbo_turtle/_abaqus_python/test_vertices.py
+++ turbo_turtle/_abaqus_python/test_vertices.py
@@ -14,7 +14,7 @@ filename = inspect.getfile(lambda: None)
 basename = os.path.basename(filename)
 parent = os.path.dirname(filename)
 sys.path.insert(0, parent)
-import vertices 
+import vertices
 
 
 class TestVertices(unittest.TestCase):
@@ -30,4 +30,14 @@ class TestVertices(unittest.TestCase):
         ]
         for coordinates, expected, rtol, atol in tests:
             bools = vertices._compare_xy_values(coordinates, rtol=rtol, atol=atol)
+            assert bools == expected
+
+    def test_compare_euclidean_distance(self):
+        tests = [
+            (numpy.array([[0, 0], [1, 0]]), 0.1, [False, True]),
+            (numpy.array([[0, 0], [1, 0]]), 10., [False, False]),
+            (numpy.array([[0, 0], [1, 0]]), 1.0, [False, False])
+        ]
+        for coordinates, euclidean_distance, expected in tests:
+            bools = vertices._compare_euclidean_distance(coordinates, euclidean_distance)
             assert bools == expected
