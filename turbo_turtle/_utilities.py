fd1722cccec3a835f3ec6c3bf2fb1dd01356469b
Kyle Brindley <kbrindley@lanl.gov>
2024-02-14 12:58:48 -0700

    MAINT: require a boolean when checking for the cubit flag

--- turbo_turtle/_utilities.py
+++ turbo_turtle/_utilities.py
@@ -117,7 +117,7 @@ def set_wrappers_and_command(args):
     :rtype: tuple
     """
     keys = vars(args).keys()
-    if "cubit" in keys and args.cubit:
+    if "cubit" in keys and args.cubit is True:
         command = find_command_or_exit(args.cubit_command)
         cubit_bin = find_cubit_bin([command])
         cubitx = cubit_bin / "cubitx"
