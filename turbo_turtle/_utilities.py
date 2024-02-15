6bb1448ed2ef728a0b869ed871650386fdfecdf3
Kyle Brindley <kbrindley@lanl.gov>
2024-02-15 12:47:23 -0700

    WIP: syntax fix

--- turbo_turtle/_utilities.py
+++ turbo_turtle/_utilities.py
@@ -117,9 +117,9 @@ def set_wrappers_and_command(args):
     :rtype: tuple
     """
     keys = vars(args).keys()
+    # TODO: remove deprecated cubit flag
+    # https://re-git.lanl.gov/aea/python-projects/turbo-turtle/-/issues/130
     if ("backend" in keys and args.backend == "cubit") or \
-       # TODO: remove deprecated cubit flag
-       # https://re-git.lanl.gov/aea/python-projects/turbo-turtle/-/issues/130
        ("cubit" in keys and args.cubit is True):
         command = find_command_or_exit(args.cubit_command)
         cubit_bin = find_cubit_bin([command])
