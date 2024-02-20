bae4ea8cece89840f1fec5cc3629e549c7836f8f
Kyle Brindley <kbrindley@lanl.gov>
2024-02-20 13:43:06 -0700

    TST: cubit wrappers geometry unit test draft

---
+++ turbo_turtle/tests/test_cubit_wrappers.py
@@ -0,0 +1,43 @@
+import copy
+import argparse
+from unittest.mock import patch
+
+import pytest
+
+from turbo_turtle import _settings
+from turbo_turtle import _cubit_wrappers
+
+
+cubit_command = "/dummy/command/cubit"
+
+geometry_keywords = {
+    "planar": False,
+    "part_name": ["part_name"],
+    "unit_conversion": 1.,
+    "euclidean_distance": 1.,
+    "delimiter": ",",
+    "header_lines": 0,
+    "revolution_angle": 360.,
+    "y_offset": 0.
+}
+geometry_namespace = copy.deepcopy(geometry_keywords)
+geometry_namespace.update({"input_file": "input_file", "output_file": "output_file"})
+
+wrapper_tests = {
+    "geometry": (
+        "geometry",
+        geometry_namespace,
+        ("input_file", "output_file"),
+        geometry_keywords
+    ),
+}
+
+
+@pytest.mark.parametrize("subcommand, namespace, positional, keywords",
+                         wrapper_tests.values(), ids=wrapper_tests.keys())
+def test_cubit_wrappers(subcommand, namespace, positional, keywords):
+    args = argparse.Namespace(**namespace)
+    with patch("turbo_turtle._cubit_python.geometry") as mock_geometry:
+        subcommand_wrapper = getattr(_cubit_wrappers, subcommand)
+        subcommand_wrapper(args, cubit_command)
+    mock_geometry.assert_called_once_with(*positional, **keywords)
