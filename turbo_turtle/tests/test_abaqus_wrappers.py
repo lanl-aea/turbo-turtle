0310a341bc76d2b64ae8c8e012db7be8fdba309e
Kyle Brindley <kbrindley@lanl.gov>
2024-02-20 12:19:02 -0700

    MAINT: reduce common namespace elements to common variable

--- turbo_turtle/tests/test_abaqus_wrappers.py
+++ turbo_turtle/tests/test_abaqus_wrappers.py
@@ -1,3 +1,4 @@
+import copy
 import argparse
 from unittest.mock import patch
 
@@ -7,7 +8,19 @@ from turbo_turtle import _settings
 from turbo_turtle import _abaqus_wrappers
 
 
-
+namespace_sparse = {
+    "input_file": "input_file",
+    "output_file": "output_file",
+    "x_angle": 0.,
+    "y_angle": 0.,
+    "z_angle": 0.,
+    "image_size": [1, 2],
+    "model_name": "model_name",
+    "part_name": None,
+    "color_map": "color_map"
+}
+namespace_full = copy.deepcopy(namespace_sparse)
+namespace_full.update({"part_name": "part_name"}),
 expected_options_sparse = [
     "command",
     "--input-file",
@@ -20,44 +33,25 @@ expected_options_sparse = [
     "--color-map"
 ]
 image = {
-    "part-name": (
-        argparse.Namespace(
-            input_file="input_file",
-            output_file="output_file",
-            x_angle=0.,
-            y_angle=0.,
-            z_angle=0.,
-            image_size=[1, 2],
-            model_name="model_name",
-            part_name="part_name",
-            color_map="color_map"
-        ),
-        expected_options_sparse + ["--part-name"]
-    ),
     "no part-name": (
-        argparse.Namespace(
-            input_file="input_file",
-            output_file="output_file",
-            x_angle=0.,
-            y_angle=0.,
-            z_angle=0.,
-            image_size=[1, 2],
-            model_name="model_name",
-            part_name=None,
-            color_map="color_map"
-        ),
+        namespace_sparse,
         expected_options_sparse
+    ),
+    "part-name": (
+        namespace_full,
+        expected_options_sparse + ["--part-name"]
     ),
 }
 
 
 @pytest.mark.parametrize("namespace, expected_options", image.values(), ids=image.keys())
 def test_image(namespace, expected_options):
+    args = argparse.Namespace(**namespace)
     with patch("turbo_turtle._utilities.run_command") as mock_run:
-        _abaqus_wrappers.image(namespace, "command")
+        _abaqus_wrappers.image(args, "command")
     mock_run.assert_called_once()
     command_string = mock_run.call_args[0][0]
     for option in expected_options:
         assert option in command_string
-    if namespace.part_name is None:
+    if "part_name" in args and args.part_name is None:
         assert "--part-name" not in command_string
