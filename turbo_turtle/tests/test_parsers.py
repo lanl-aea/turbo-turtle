0772bc16d9511df363d8a74260cbb1fef3a83aa4
Kyle Brindley <kbrindley@lanl.gov>
2024-02-21 11:52:42 -0700

    MAINT: parameterize with outcome

--- turbo_turtle/tests/test_parsers.py
+++ turbo_turtle/tests/test_parsers.py
@@ -1,9 +1,25 @@
+from unittest.mock import patch
+from contextlib import nullcontext as does_not_raise
+
 import pytest
 import numpy
 
 from turbo_turtle._abaqus_python import parsers
 
 
-def test_positive_float():
-    assert numpy.isclose(parsers.positive_float("1."), 1.)
-    assert numpy.isclose(parsers.positive_float("0."), 0.)
+positive_float = {
+    "zero": ("0.", 0., does_not_raise()),
+    "one": ("1.", 1., does_not_raise()),
+}
+
+
+@pytest.mark.parametrize("input_string, expected_float, outcome",
+                         positive_float.values(),
+                         ids=positive_float.keys())
+def test_positive_float(input_string, expected_float, outcome):
+    with outcome:
+        try:
+            argument = parsers.positive_float(input_string)
+            assert numpy.isclose(argument, expected_float)
+        finally:
+            pass
