3d14d5981765e7759739c2413fbff397ff70278d
Kyle Brindley <kbrindley@lanl.gov>
2024-02-21 11:48:40 -0700

    MAINT: float comparisons with numpy

--- turbo_turtle/tests/test_parsers.py
+++ turbo_turtle/tests/test_parsers.py
@@ -1,8 +1,9 @@
 import pytest
+import numpy
 
 from turbo_turtle._abaqus_python import parsers
 
 
 def test_positive_float():
-    assert(parsers.positive_float("1.") == 1.)
-    assert(parsers.positive_float("0.") == 0.)
+    assert numpy.isclose(parsers.positive_float("1."), 1.)
+    assert numpy.isclose(parsers.positive_float("0."), 0.)
