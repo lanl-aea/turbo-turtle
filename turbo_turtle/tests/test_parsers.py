8074ace899d0007f662e12766643ad582efc74bd
Kyle Brindley <kbrindley@lanl.gov>
2024-02-21 11:56:06 -0700

    TST: unit test the positive integer type behavior

--- turbo_turtle/tests/test_parsers.py
+++ turbo_turtle/tests/test_parsers.py
@@ -26,3 +26,23 @@ def test_positive_float(input_string, expected_float, outcome):
             assert numpy.isclose(argument, expected_float)
         finally:
             pass
+
+
+positive_int = {
+    "zero": ("0", 0, does_not_raise()),
+    "one": ("1", 1, does_not_raise()),
+    "negative": ("-1", None, pytest.raises(argparse.ArgumentTypeError)),
+    "string": ("negative_one", None, pytest.raises(argparse.ArgumentTypeError)),
+}
+
+
+@pytest.mark.parametrize("input_string, expected_int, outcome",
+                         positive_int.values(),
+                         ids=positive_int.keys())
+def test_positive_int(input_string, expected_int, outcome):
+    with outcome:
+        try:
+            argument = parsers.positive_int(input_string)
+            assert argument == expected_int
+        finally:
+            pass
