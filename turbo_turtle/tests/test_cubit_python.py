68e12feab2a1505f86fdd4c360a49d464ebad276
Kyle Brindley <kbrindley@lanl.gov>
2024-02-22 11:48:09 -0700

    TST: unit tests using the Cubit Python API

--- turbo_turtle/tests/test_cubit_python.py
+++ turbo_turtle/tests/test_cubit_python.py
@@ -1,10 +1,26 @@
+from contextlib import nullcontext as does_not_raise
+
 import pytest
+cubit = pytest.importorskip("cubit", reason="Could not import Cubit")
 
-cubit = pytest.importorskip("badpackage", reason="Could not import Cubit")
+from turbo_turtle import _cubit_python
 
 
-def test_cubit_command_or_exception():
-    pass
+cubit_command_or_exception = {
+    "good command": ("reset aprepro", does_not_raise()),
+    "bad command": ("definitetlynotacubitcommand", pytest.raises(RuntimeError)),
+}
+
+
+@pytest.mark.parametrize("command, outcome",
+                         cubit_command_or_exception.values(),
+                         ids=cubit_command_or_exception.keys())
+def test_cubit_command_or_exception(command, outcome):
+    with outcome:
+        try:
+            success = _cubit_python.cubit_command_or_exception(command)
+        finally:
+            pass
 
 def test_cubit_command_or_exit():
     pass
