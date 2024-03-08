4d163fc84dba91fcc6ebc800dd46cd13a6b6b9a8
Thomas Phillip Roberts <tproberts@lanl.gov>
2024-03-08 10:28:28 -0700

    TEST: forgot to update the expected output for the print-abaqus-path test

--- turbo_turtle/tests/test_main.py
+++ turbo_turtle/tests/test_main.py
@@ -32,7 +32,7 @@ def test_print_abaqus_path(capsys):
     """Test the print-abaqus-path subcommand behavior"""
 
     # Test printing behavior
-    expected_output = f"{_settings._abaqus_python_abspath}\n"
+    expected_output = f"{_settings._abaqus_python_parent_abspath}\n"
     main._print_abaqus_path_location()
     returned_output = capsys.readouterr()
     assert expected_output == returned_output.out
