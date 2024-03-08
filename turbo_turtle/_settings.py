f396b468670f4eb57bee77dcb1b0af2b84f5075e
Thomas Phillip Roberts <tproberts@lanl.gov>
2024-03-08 07:59:35 -0700

    WIP: create a new settings variable to identify the parent dir when the Abaqus python scripts get moved to a new package dir

--- turbo_turtle/_settings.py
+++ turbo_turtle/_settings.py
@@ -4,7 +4,8 @@ import pathlib
 _project_name = "Turbo Turtle"
 _project_name_short = "turbo-turtle"
 _project_root_abspath = pathlib.Path(__file__).parent.resolve()
-_abaqus_python_abspath = _project_root_abspath / "_abaqus_python"
+_abaqus_python_parent_abspath = _project_root_abspath / "_abaqus_python"
+_abaqus_python_abspath = _abaqus_python_parent_abspath / "turbo_turtle_abaqus"
 _installed_docs_index = _project_root_abspath / "docs/index.html"
 _default_abaqus_options = ["abaqus", "abq2023"]
 _default_cubit_options = ["cubit"]
