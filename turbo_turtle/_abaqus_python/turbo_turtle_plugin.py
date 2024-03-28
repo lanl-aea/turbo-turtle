f5be85f3d3341316ebb3ca1718c12ff44bfdf7c5
Thomas Phillip Roberts <tproberts@lanl.gov>
2024-03-28 07:22:21 -0600

    FEAT: utilize built-in features of the plugin registration command to display the help message and link to gui documentation in Abaqus/CAE

--- turbo_turtle/_abaqus_python/turbo_turtle_plugin.py
+++ turbo_turtle/_abaqus_python/turbo_turtle_plugin.py
@@ -2,7 +2,17 @@
 
 This script defines Abaqus CAE plugin toolkit options
 """
+import inspect
+import os
+import sys
+
+filename = inspect.getfile(lambda: None)
+parent = os.path.dirname(filename)
+sys.path.insert(0, parent)
+from turbo_turtle_abaqus import _mixed_settings
+
 from abaqusGui import *
+
 
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()  # Do this only once
 
@@ -11,11 +21,17 @@ toolset.registerKernelMenuButton(
     buttonText='Turbo-Turtle|Geometry',
     moduleName='turbo_turtle_abaqus.geometry', functionName='_gui()',
     applicableModules=('Part', ),
-    icon=afxCreateIcon('turboTurtleIcon.png'))
+    icon=afxCreateIcon('turboTurtleIcon.png'),
+    helpUrl=_mixed_settings._gui_docs_file,
+    author=_mixed_settings._author,
+    description=_mixed_settings._geometry_gui_help_string)
 
 # Partition Gui Plugin
 toolset.registerKernelMenuButton(
     buttonText='Turbo-Turtle|Partition',
     moduleName='turbo_turtle_abaqus.partition', functionName='_gui()',
     applicableModules=('Part', ),
-    icon=afxCreateIcon('turboTurtleIcon.png'))
+    icon=afxCreateIcon('turboTurtleIcon.png'),
+    helpUrl=_mixed_settings._gui_docs_file,
+    author=_mixed_settings._author,
+    description=_mixed_settings._partition_gui_help_string)
