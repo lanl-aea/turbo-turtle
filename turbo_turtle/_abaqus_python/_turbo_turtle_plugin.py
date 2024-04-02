116d92f382e596729bb8647250c98940b794dff9
Thomas Phillip Roberts <tproberts@lanl.gov>
2024-04-02 15:09:26 -0600

    MAINT: add cylinder to registered gui plugins

--- turbo_turtle/_abaqus_python/_turbo_turtle_plugin.py
+++ turbo_turtle/_abaqus_python/_turbo_turtle_plugin.py
@@ -16,6 +16,16 @@ from abaqusGui import *
 
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()  # Do this only once
 
+# Cylinder Gui Plugin
+toolset.registerKernelMenuButton(
+    buttonText='Turbo-Turtle|Cylinder',
+    moduleName='turbo_turtle_abaqus.cylinder', functionName='_gui()',
+    applicableModules=('Part', ),
+    icon=afxCreateIcon('turboTurtleIcon.png'),
+    helpUrl=_mixed_settings._gui_docs_file,
+    author=_mixed_settings._author,
+    description=_mixed_settings._cylinder_gui_help_string)
+
 # Geometry Gui Plugin
 toolset.registerKernelMenuButton(
     buttonText='Turbo-Turtle|Geometry',
