f36e36915d563e4207df02fcb57447f887e73edc
Kyle Brindley <kbrindley@lanl.gov>
2024-04-04 14:43:37 -0600

    MAINT: use semi-private main module

--- turbo_turtle/tests/test_main.py
+++ turbo_turtle/tests/test_main.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 import pytest
 
-from turbo_turtle import main
+from turbo_turtle import _main
 from turbo_turtle import _settings
 
 
@@ -10,13 +10,13 @@ def test_docs():
     """Test the docs subcommand behavior"""
     with patch("webbrowser.open") as mock_webbrowser_open, \
          patch("pathlib.Path.exists", return_value=True):
-        main._docs()
+        _main._docs()
         # Make sure the correct type is passed to webbrowser.open
         mock_webbrowser_open.assert_called_with(str(_settings._installed_docs_index))
 
     with patch("webbrowser.open") as mock_webbrowser_open, \
          patch("pathlib.Path.exists", return_value=True):
-        main._docs(print_local_path=True)
+        _main._docs(print_local_path=True)
         mock_webbrowser_open.assert_not_called()
 
     # Test the "unreachable" exit code used as a sign-of-life that the installed package structure assumptions in
@@ -24,7 +24,7 @@ def test_docs():
     with patch("webbrowser.open") as mock_webbrowser_open, \
          patch("pathlib.Path.exists", return_value=False), \
          pytest.raises(SystemExit):
-        main._docs(print_local_path=True)
+        _main._docs(print_local_path=True)
     mock_webbrowser_open.assert_not_called()
 
 
@@ -33,6 +33,6 @@ def test_print_abaqus_path(capsys):
 
     # Test printing behavior
     expected_output = f"{_settings._abaqus_python_parent_abspath}\n"
-    main._print_abaqus_path_location()
+    _main._print_abaqus_path_location()
     returned_output = capsys.readouterr()
     assert expected_output == returned_output.out
