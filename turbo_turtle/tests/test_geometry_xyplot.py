637c9d72fe415ef815fd93b36d7d7755fc354e24
Kyle Brindley <kbrindley@lanl.gov>
2024-04-04 15:40:18 -0600

    TST: minimal sign-of-life on pass through arguments

---
+++ turbo_turtle/tests/test_geometry_xyplot.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+import pytest
+
+from turbo_turtle import geometry_xyplot
+from turbo_turtle._abaqus_python.turbo_turtle_abaqus import parsers
+
+
+def test_main():
+    kwargs = {}
+    expected_call_kwargs = {
+        "unit_conversion": parsers.geometry_xyplot_defaults["unit_conversion"],
+        "euclidean_distance": parsers.geometry_xyplot_defaults["euclidean_distance"],
+        "y_offset": parsers.geometry_xyplot_defaults["y_offset"],
+        "rtol": parsers.geometry_xyplot_defaults["rtol"],
+        "atol": parsers.geometry_xyplot_defaults["atol"],
+        "no_markers": parsers.geometry_xyplot_defaults["no_markers"],
+        "annotate": parsers.geometry_xyplot_defaults["annotate"],
+        "scale": parsers.geometry_xyplot_defaults["scale"]
+    }
+    with patch("turbo_turtle._abaqus_python.turbo_turtle_abaqus._mixed_utilities.return_genfromtxt_or_exit"), \
+         patch("turbo_turtle.geometry_xyplot.geometry_xyplot") as mock_plot:
+        geometry_xyplot._main(["dummy.in"], ["dummy.out"], **kwargs)
+    assert mock_plot.call_args.kwargs == expected_call_kwargs
