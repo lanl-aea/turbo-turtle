bc5c65f9c9d7dce54a781d92438454463dc8a879
Kyle Brindley <kbrindley@lanl.gov>
2024-04-25 16:14:03 -0600

    WIP: add partial import comparisons

--- import_turbo_turtle.py
+++ import_turbo_turtle.py
@@ -1,1 +1,25 @@
-import turbo_turtle
+import argparse
+import importlib
+
+
+def get_parser() -> argparse.Namspace:
+    parser = argparse.ArgumentParser("Import modules of turbo turtle. Always performs package import")
+    parser.add_argument(
+        "MODULE",
+        nargs="*",
+        help="turbo_turtle case-sensitive module names"
+    )
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    import turbo_turtle
+    for module in args.MODULE:
+        importlib.import_module(f"turbo_turtle.{module}")
+
+
+if __name__ == "__main__":
+    main()
