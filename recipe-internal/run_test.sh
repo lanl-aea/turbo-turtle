fa30d5a16b4a539a67a12a93da3e00ef44304def
Kyle Brindley <kbrindley@lanl.gov>
2024-11-25 10:15:10 -0700

    WIP: pytest command line syntax

--- recipe-internal/run_test.sh
+++ recipe-internal/run_test.sh
@@ -1,4 +1,4 @@
 pip check
 cd $SP_DIR/$PKG_NAME
-pytest -vvv -n 4 --ignore=_abaqus_python --abaqus-command /apps/abaqus/Commands/abq2023 --cubit-command /apps/Cubit-16.16/cubit
+pytest -vvv -n 4 --ignore=_abaqus_python --abaqus-command=/apps/abaqus/Commands/abq2023 --cubit-command=/apps/Cubit-16.16/cubit
 /apps/abaqus/Commands/abq2023 abq2023 python -m unittest discover _abaqus_python/turbo_turtle_abaqus --verbose
