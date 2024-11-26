8b4a74c021a98c7eef657ad6e1faf44f93875ed9
Kyle Brindley <kbrindley@lanl.gov>
2024-11-26 10:20:41 -0700

    TST: run the full pytest suite for internal conda-build recipe testing. Windows CI server doesn't have third-party software installed, match test suite to external conda-build recipe

--- recipe-internal/run_test.bat
+++ recipe-internal/run_test.bat
@@ -1,4 +1,4 @@
 pip check
 cd %SP_DIR%\%PKG_NAME%
-pytest -vvv -n 4 --ignore=_abaqus_python
+pytest -vvv -n 4 --ignore=_abaqus_python -m "not require_third_party"
 abq2024 python -m unittest discover _abaqus_python\turbo_turtle_abaqus --verbose
