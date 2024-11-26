450b1d4a8e68ada6fd530f462caad92a08876e56
Kyle Brindley <kbrindley@lanl.gov>
2024-11-26 11:19:39 -0700

    MAINT: add internal/external path options for package documentation

--- package_documentation.py
+++ package_documentation.py
@@ -7,7 +7,10 @@ sp_dir = pathlib.Path(os.getenv("SP_DIR")).resolve()
 pkg_name = os.getenv("PKG_NAME")
 
 man_path = pathlib.Path("build/docs/man/turbo-turtle.1").resolve()
-html_path = pathlib.Path("build/docs/html").resolve()
+html_path_external = pathlib.Path("build/docs/html").resolve()
+html_path_internal = pathlib.Path("build/docs/html-internal").resolve()
+html_path = html_path_external if html_path_external.exists() else html_path_internal
+# TODO: figure out the setuptools and setuptools_scm solution for including data files
 # FIXME: setuptools claims that these should be included by default, but they aren't
 readme_path = pathlib.Path("README.rst").resolve()
 pyproject = pathlib.Path("pyproject.toml").resolve()
@@ -28,5 +31,10 @@ for destination, source in new_paths:
         shutil.copy2(source, destination, follow_symlinks=True)
     else:
         destination.mkdir(parents=True, exist_ok=True)
-        shutil.copytree(source, destination, symlinks=False, dirs_exist_ok=True,
-                        ignore=shutil.ignore_patterns(".doctrees", "*.doctree", ".buildinfo"))
+        shutil.copytree(
+            source,
+            destination,
+            symlinks=False,
+            dirs_exist_ok=True,
+            ignore=shutil.ignore_patterns(".doctrees", "*.doctree", ".buildinfo"),
+        )
